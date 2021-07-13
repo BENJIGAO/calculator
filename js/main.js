081db3963b46c0c70f00b3fb1e0718ef643113d7
Brian Gao <bhgao20014@gmail.com>
2021-07-12 20:27:39 -0400

    Added basic adg, subtract, multiple, and divide function; added basic non-functioning calculator UI

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
