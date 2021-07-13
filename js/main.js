df1f5d1b47a8f9031b62fae554071bb8cc9ad4b1
Brian Gao <bhgao20014@gmail.com>
2021-07-13 12:46:27 -0400

    Added plus, minus, multiply, and divide functions; experimenting to solve functionality issue

--- js/main.js
+++ js/main.js
@@ -31,13 +31,41 @@ function operate(num1, num2, operator) {
     }
 }
 
+function main() {
+    let a = 5;
+    const plusBtn = document.getElementById('plus-btn');
+    plusBtn.addEventListener('click', doOperation);
+
+    
+    activateBtns();
+    
+
+}
+
+
 function activateBtns() {
+    
     const nums = document.querySelectorAll('.num');
     nums.forEach((num) => num.addEventListener('click', populateDisplay));
+    const subtractBtn = document.getElementById('subtract-btn');
+    subtractBtn.addEventListener('click', doOtherOperation)
+    
+}
+
+function doOtherOperation() {
+    console.log(a);
+}
+
+function doOperation() {
+    console.log(a);
+    return;
 }
 
 function populateDisplay(e) {
     document.getElementById('number-display').textContent += e.target.textContent;
+    console.log(a);
 }
 
-activateBtns();
+main();
+
+
