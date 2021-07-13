8c9d08881f21ef2b570f98e02367bf861ab5f887
Brian Gao <bhgao20014@gmail.com>
2021-07-13 17:26:00 -0400

    Fixed bug that made first typed number after pressing an operator invisible

--- js/main.js
+++ js/main.js
@@ -28,14 +28,6 @@ function operate(num1, num2, operator) {
     }
 }
 
-function activateBtns() {
-    const nums = document.querySelectorAll('.num');
-    nums.forEach(num => num.addEventListener('click', populateDisplay));
-    const operatorBtns = document.querySelectorAll('.operator');
-    operatorBtns.forEach(operatorBtn => operatorBtn.addEventListener('click', doOperation))
-    
-}
-
 function doOperation(e) {
     const calcDisplay = document.getElementById('number-display');
     let result = operate(
@@ -46,6 +38,26 @@ function doOperation(e) {
     calcDisplay.dataset.num1 = calcDisplay.textContent = result;
     calcDisplay.dataset.operator = e.target.getAttribute('id');
     calcDisplay.dataset.num2 = '0';
+    const nums = document.querySelectorAll('.num');
+    nums.forEach(num => {
+        num.removeEventListener('click', populateDisplay);
+        num.addEventListener('click', resetDisplay);
+        num.addEventListener('click', populateDisplay);
+    })
+}
+
+function resetDisplay() {
+    const nums = document.querySelectorAll('.num');
+    document.getElementById('number-display').textContent = '';
+    nums.forEach(num => num.removeEventListener('click', resetDisplay))
+}
+
+function activateBtns() {
+    const nums = document.querySelectorAll('.num');
+    nums.forEach(num => num.addEventListener('click', populateDisplay));
+    const operatorBtns = document.querySelectorAll('.operator');
+    operatorBtns.forEach(operatorBtn => operatorBtn.addEventListener('click', doOperation))
+    
 }
 
 function populateDisplay(e) {
