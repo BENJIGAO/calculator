c56c3aba2469184802b92a341a56b65b635ed683
Brian Gao <bhgao20014@gmail.com>
2021-07-13 19:46:00 -0400

    Implemented clear, equals, and switch signs button for calculator

--- js/main.js
+++ js/main.js
@@ -1,3 +1,70 @@
+activateBtns();
+
+function activateBtns() {
+    const nums = document.querySelectorAll('.num');
+    nums.forEach(num => num.addEventListener('click', populateDisplay));
+    const operatorBtns = document.querySelectorAll('.operator');
+    operatorBtns.forEach(operatorBtn => operatorBtn.addEventListener('click', doOperation))
+    const clearBtn = document.getElementById('clear');
+    clearBtn.addEventListener('click', resetCalculator);
+    const switchSignBtn = document.getElementById('switch-sign');
+    switchSignBtn.addEventListener('click', switchSign);
+}
+
+function switchSign() {
+    const calcDisplay = document.getElementById('number-display');
+    calcDisplay.dataset.num2 = String(+calcDisplay.dataset.num2 * -1);
+    calcDisplay.textContent = calcDisplay.dataset.num2;
+}
+
+function doOperation(e) {
+    const calcDisplay = document.getElementById('number-display');
+    // What happens if user presses an two operators consecutively
+    if (calcDisplay.dataset.num2 == '0') return;
+    let result = operate(
+        +calcDisplay.dataset.num1,
+        +calcDisplay.dataset.num2,
+        calcDisplay.dataset.operator
+    );
+    updateDisplay(calcDisplay, result, e.target.getAttribute('id'));
+}
+
+function updateDisplay(display, result, operator) {
+    display.textContent = result;
+    if (result === 'ERROR') result = '0';
+    display.dataset.num1 = result
+    // Makes sure operator != equals sign
+    if (operator) display.dataset.operator = operator;
+    display.dataset.num2 = '0';
+    const nums = document.querySelectorAll('.num');
+    nums.forEach(num => {
+        num.removeEventListener('click', populateDisplay);
+        num.addEventListener('click', resetDisplay);
+        num.addEventListener('click', populateDisplay);
+    })
+}
+
+function resetDisplay() {
+    const nums = document.querySelectorAll('.num');
+    document.getElementById('number-display').textContent = '';
+    nums.forEach(num => num.removeEventListener('click', resetDisplay))
+}
+
+function resetCalculator() {
+    const calcDisplay = document.getElementById('number-display');
+    calcDisplay.textContent = calcDisplay.dataset.num1 = calcDisplay.dataset.num1 = '0';
+    calcDisplay.dataset.operator = '+';
+}
+
+function populateDisplay(e) {
+    const calcDisplay = document.getElementById('number-display');
+    // Condition for initial state of calculator
+    if (calcDisplay.textContent == '0') calcDisplay.textContent = '';
+    const newNum = e.target.textContent;
+    calcDisplay.textContent += newNum;
+    calcDisplay.dataset.num2 += newNum;
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -20,7 +87,6 @@ function operate(num1, num2, operator) {
             return add(num1, num2);
         case '-':
             return subtract(num1, num2);
-          
         case '*':
             return multiply(num1, num2);
         case '/':
@@ -28,54 +94,5 @@ function operate(num1, num2, operator) {
     }
 }
 
-function doOperation(e) {
-    const calcDisplay = document.getElementById('number-display');
-    // What happens if user presses an two operators consecutively
-    if (calcDisplay.dataset.num2 == '0') return;
-    let result = operate(
-        +calcDisplay.dataset.num1,
-        +calcDisplay.dataset.num2,
-        calcDisplay.dataset.operator
-    );
-    updateDisplay(calcDisplay, result, e.target.getAttribute('id'));
-}
-
-function updateDisplay(display, result, operator) {
-    display.textContent = result;
-    if (result === 'ERROR') result = '0';
-    display.dataset.num1 = result
-    display.dataset.operator = operator;
-    display.dataset.num2 = '0';
-    const nums = document.querySelectorAll('.num');
-    nums.forEach(num => {
-        num.removeEventListener('click', populateDisplay);
-        num.addEventListener('click', resetDisplay);
-        num.addEventListener('click', populateDisplay);
-    })
-}
-
-function resetDisplay() {
-    const nums = document.querySelectorAll('.num');
-    document.getElementById('number-display').textContent = '';
-    nums.forEach(num => num.removeEventListener('click', resetDisplay))
-}
-
-function activateBtns() {
-    const nums = document.querySelectorAll('.num');
-    nums.forEach(num => num.addEventListener('click', populateDisplay));
-    const operatorBtns = document.querySelectorAll('.operator');
-    operatorBtns.forEach(operatorBtn => operatorBtn.addEventListener('click', doOperation))
-    
-}
-
-function populateDisplay(e) {
-    const calcDisplay = document.getElementById('number-display');
-    if (calcDisplay.textContent == '0') calcDisplay.textContent = '';
-    const newNum = e.target.textContent;
-    calcDisplay.textContent += newNum;
-    calcDisplay.dataset.num2 += newNum;
-}
-
-activateBtns();
 
 
